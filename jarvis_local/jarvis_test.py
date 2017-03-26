6e304f8328675060765cd58f1bcc6a008ebcde7c
rb1811 <pracherl@asu.edu>
2017-03-26 00:54:45 -0700

    code not tested

--- jarvis_local/jarvis_test.py
+++ jarvis_local/jarvis_test.py
@@ -17,97 +17,99 @@ ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 
-# if platform.system() == "Linux":
-#     current_path = "/home/"+ getpass.getuser() +"/Downloads/"
-# elif platform.system() == "Darwin":
-#     current_path = "/Users/"+ getpass.getuser() +"/Downloads/"
-# elif platform.system() == "Windows":
-#     pass
-    #add folder path structure here for windows
+if platform.system() == "Linux":
+    current_path = "/home/"+ getpass.getuser() +"/Downloads/"
+elif platform.system() == "Darwin":
+    current_path = "/Users/"+ getpass.getuser() +"/Downloads/"
+elif platform.system() == "Windows":
+    pass
+    # add folder path structure here for windows
 
 
 
 @ask.launch
 def file_browser():
     # welcome_msg = render_template('welcome')
-    welcome_msg = "Welcome Aditya, how are you ? "
+    welcome_msg = "Welcome,"+ getpass.getuser() + "! what can I do for you ?"
     print "---------------------------",welcome_msg,"---------------------------"
     return question(welcome_msg)
 
-@ask.intent("UploadDrive")
-def upload_drive():
-    msg = render_template('upload')
+@ask.intent("UploadDrive", convert={'FolderName' : str})
+def upload_drive(FolderName):
+    global current_path
+    gauth = GoogleAuth()
+    gauth.LoadCredentialsFile("credentials.txt")
+    if gauth.credentials is None:
+        # Authenticate if they're not there
+        gauth.LocalWebserverAuth()
+    elif gauth.access_token_expired:
+        # Refresh them if expired
+        gauth.Refresh()
+    else:
+        # Initialize the saved creds
+        gauth.Authorize()
+    # Save the current credentials to a file
+    gauth.SaveCredentialsFile("credentials.txt")
+
+    drive = GoogleDrive(gauth)
+    file = drive.CreateFile()
+    file.SetContentFile(join(current_path,str(FolderName)))
+    file.Upload()
     return question(msg)
-    # global current_path
-    # gauth = GoogleAuth()
-    # gauth.LoadCredentialsFile("credentials.txt")
-    # if gauth.credentials is None:
-    #     # Authenticate if they're not there
-    #     gauth.LocalWebserverAuth()
-    # elif gauth.access_token_expired:
-    #     # Refresh them if expired
-    #     gauth.Refresh()
-    # else:
-    #     # Initialize the saved creds
-    #     gauth.Authorize()
-    # # Save the current credentials to a file
-    # gauth.SaveCredentialsFile("credentials.txt")
-
-    # drive = GoogleDrive(gauth)
-    # file = drive.CreateFile()
-    # file.SetContentFile(join(current_path,recv_chat))
-    # file.Upload()
-
+    
 
 @ask.intent("OpenFolder", convert={'FolderName' : str})
 def open_folder(FolderName):
-    # msg = render_template('open')
-    # print intent['slots']['foldername']['value']
-    msg = "Opening folder " + str(FolderName)
+    global current_path
+    if isdir(join(current_path,str(FolderName))):
+        current_path =  join(current_path, str(FolderName))
+    elif isfile(join(current_path,str(FolderName))):
+        if platform.system() == "Linux":
+            os.system('xdg-open ' + join(current_path,str(FolderName)))
+        elif platform.system() == "Darwin":
+            os.system('open ' + join(current_path,str(FolderName)))
+        elif platform.system() == "Windows":
+            pass
+            #add folder open code for current_path for Windows
+    else: 
+        msg  = "I am sorry, there is no such folder by that name"
+        return question(msg)
+        
+    print "The current path ", current_path
+    msg  = "The folder" +str(FolderName)+ "is opened for you"
     return question(msg)
-    # global current_path
-    # if isdir(join(current_path,recv_chat)):
-    #     current_path =  join(current_path, recv_chat)
-    # elif isfile(join(current_path,recv_chat)):
-    #     if platform.system() == "Linux":
-    #         os.system('xdg-open ' + join(current_path,recv_chat))
-    #     elif platform.system() == "Darwin":
-    #         os.system('open ' + join(current_path,recv_chat))
-    #     elif platform.system() == "Windows":
-    #         pass
-    #         #add folder open code for current_path for Windows
-    # print "The current path ", current_path
-    # return
+    
+
 
 @ask.intent("GoBack")
 def go_back():
-    msg = render_template('goback') 
-    return question(msg)   
-    # global current_path
-    # if not current_path.rindex('/'):
-    #     current_path = '/'
-    # else:   
-    #     current_path =  current_path[:current_path.rindex('/')]
-    # print "The current path ", current_path
-    # return
-
-@ask.intent("CreateFolder")
-def create_folder():
-    msg = render_template('create')
+    global current_path
+    if not current_path.rindex('/'):
+        current_path = '/'
+    else:   
+        current_path =  current_path[:current_path.rindex('/')]
+    print "The current path ", current_path
+    msg = "This is the previous folder"
     return question(msg)
-    # global current_path
-    # if not isdir(join(current_path,recv_chat)):
-    #     os.system('mkdir '+join(current_path,recv_chat))
-    #     current_path  =  current_path
-    # else:
-    #     c.send("Hey "+user_name+ " darling there is folder already by that name please choose a different name")
-    # print "The current path ", current_path
-    # return
 
-@ask.intent("DeleteFolder")    
-def delete_folder():
-    msg = render_template('delete')
+@ask.intent("CreateFolder" , convert={'FolderName' : str})
+def create_folder(FolderName):
+    global current_path
+    if not isdir(join(current_path,recv_chat)):
+        os.system('mkdir '+join(current_path,recv_chat))
+        current_path  =  current_path
+    else:
+        msg  = "I am sorry" + + "there is folder already by that name. Please choose a different name"
+        return question(msg)
+    print "The current path ", current_path
+    msg  = "I have created a folder by the name"
     return question(msg)
+    
+
+# @ask.intent("DeleteFolder")    
+# def delete_folder():
+    
+#     return question(msg)
     # global current_path
     # if isdir(join(current_path,recv_chat)):
     #     os.system('rm -rf '+join(current_path,recv_chat))
@@ -117,7 +119,7 @@ def delete_folder():
 
 @ask.intent("RenameFolder")
 def rename_folder():
-    msg = render_template('rename')
+    
     return question(msg)
     # global current_path
     # names =  recv_chat.split(' ')
@@ -128,17 +130,6 @@ def rename_folder():
     # return
 
 
-
-# @ask.intent("AnswerIntent", convert={'first': int, 'second': int, 'third': int})
-# def answer(first, second, third):
-#     # print convert['first'], convert['second'], convert['thrid']
-#     winning_numbers = session.attributes['numbers']
-#     print winning_numbers
-#     if [first, second, third] == winning_numbers:
-#         msg = render_template('win')
-#     else:
-#         msg = render_template('lose')
-#     return question(msg)
 
 if __name__ == '__main__':
     app.run(debug=True)
